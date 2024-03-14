21414b97f6dc63a4600a67611f9083443fb474fc
Asad9340 <asadulimran1999@gmail.com>
2024-03-15 03:06:31 +0600

    responsive for mobile device

--- src/Components/Input/Input.jsx
+++ src/Components/Input/Input.jsx
@@ -21,7 +21,7 @@ function Input({ handleBtnClick }) {
     }
   };
   return (
-    <div className="grid gap-4 md:gap-10 grid-cols-12 mx-4 md:mx-10">
+    <div className="flex flex-col md:grid gap-4 md:gap-10 grid-cols-12 mx-4 md:mx-10">
       <div className="col-span-6">
         <input
           className="w-full h-12 pl-2 rounded-lg duration-200 border border-gray-900 hover:bg-gray-200"
